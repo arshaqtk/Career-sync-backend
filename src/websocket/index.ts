f27cc2f7cecb3a81fa6627db91192ed1deb91d33
muhammed  arshaq tk <arshaqtk4@gmail.com>
2026-01-16 10:56:27 +0530

    updated socket cors

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -4,7 +4,7 @@ import { Server as httpServer } from "http"
 export const initSocket=(server:httpServer)=>{
     const io=new Server(server,{
         cors:{
-           origin: "http://localhost:5173",
+           origin: "https://career-sync-frontend.vercel.app",
     credentials: true,
         }
     })
